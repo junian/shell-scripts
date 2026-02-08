2837fabce7bb2667d9f80b5bd25a82ef514c34e6
Junian Triajianto <author@junian.dev>
2026-02-08 12:54:09 +0700

    refactor(ffmpeg): Improve blur-pillarbox filter readability

--- ffmpeg/ffmpeg-blur-pillarbox.sh
+++ ffmpeg/ffmpeg-blur-pillarbox.sh
@@ -12,9 +12,18 @@ function ffmpeg-blur-pillarbox() {
   fi
 
   ffmpeg -i "$1" \
-    -vf "split[original][copy]; \
-         [copy]scale=ih*16/9:-1,crop=h=iw*9/16,gblur=sigma=20[blurred]; \
-         [blurred][original]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
+    -vf "
+      split[original][copy];
+
+      [copy]
+        scale=ih*16/9:-1
+       ,crop=h=iw*9/16
+       ,gblur=sigma=20
+      [copy];
+
+      [copy][original]
+      overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2
+    " \
     "$2"
 }
 
